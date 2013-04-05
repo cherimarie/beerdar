7ed3e5b4e97ea9cc76baff9e390483a7d2ce2eb8
cherimarie <cherimarie@gmail.com>
2013-04-05 16:44:42 -0700

    Create an app, set up Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beerdar::Application.config.secret_token = '6b071ef7be92c9ea28baf1cf4e05c5dac51b700b7c5198b6b4df0e5d21d8a1ab6e00700e4d61a2e493b6d6c6f4b9959d644e694d1ee8cada39f2b528e88ad2f9'
