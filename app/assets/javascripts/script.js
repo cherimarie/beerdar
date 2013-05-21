1a397e90c0fab9311a97e8304a4252cddb7c86f4
cherimarie <cherimarie@gmail.com>
2013-05-21 09:19:48 -0700

    Tidying up code- no real changes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -17,90 +17,89 @@
     lngi = position.coords.longitude;
 
     //just to show results
-
     var latdump = document.getElementById("latlng");
     latdump.innerHTML = "your location: " + lati + lngi;
-  // Create and load map
-$('#map').mapbox('beerdar.map-97fds4u6', function(map, tilejson)
-{
 
-    alert(lati);
-    map.setZoomRange(10, 19);
-    var markerLayer = mapbox.markers.layer();
-    mapbox.markers.interaction(markerLayer);
+    // Create and load map
+    $('#map').mapbox('beerdar.map-97fds4u6', function(map, tilejson)
+    {
 
-    markerLayer.add_feature({
-    geometry: {coordinates: [-122.32, 47.61]},
-    properties: {'marker-color': '#000',
-                  'marker-size': "medium",
-                  'marker-symbol': "bus",
-                  title: 'Bar One',
-                  description: 'Happy Hour blah blah.'}
-    });
-    markerLayer.add_feature({
-    geometry: {coordinates: [-122.4, 47.58]},
-    properties: {'marker-color': '#000',
-                  'marker-size': "medium",
-                  'marker-symbol': "bus",
-                  title: 'Bar Two',
-                  description: 'Happy Hour c.'}
-    });
-    markerLayer.add_feature({
-    geometry: {coordinates: [-122.38, 47.55]},
-    properties: {'marker-color': '#000',
-                'marker-size': "medium",
-                'marker-symbol': "bus",
-                title: 'Bar Three',
-                description: 'Happy Hour rulezzzz'}
-    });
-   markerLayer.add_feature({
-    geometry: {coordinates: [lngi, lati]},
-    properties: {'marker-color': '#E01B6A',
-                'marker-size': "medium",
-                'marker-symbol': "bus",
-                title: 'You',
-                description: 'Here you are.'}
+        map.setZoomRange(10, 19);
+        var markerLayer = mapbox.markers.layer();
+        mapbox.markers.interaction(markerLayer);
 
-    });
-
-    map.addLayer(markerLayer)
-      .setExtent(markerLayer.extent());
-
-
-    // Add share control
-    mapbox.share().map(map).add();
-
-    // Set title and description from tilejson
-    document.title = tilejson.name;
-    $('h1.map-title').text(tilejson.name);
-    $('p.description').text(tilejson.description);
-
-
-    var container = $('#markerfilters');
-    $.each(tilejson.markers.markers(), function(index, m) {
-        var s = m.data.properties['marker-symbol'];
-
-        if (container.find('[href="#' + s + '"]').length) return;
-
-        var el = $(document.createElement('a'))
-            .addClass('markerfilter')
-            .attr('href', '#' + s)
-            .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-'+s+'+000000.png)')
-            .bind('click', filter);
-        container.append(el);
-    });
-
-
-    $('[href="#all"]').bind('click', filter);
-
-
-    function filter(e) {
-        container.find('a').removeClass('selected');
-        var id = $(this).addClass('selected').attr('href').replace('#', '');
-        tilejson.markers.filter(function(feature) {
-            return feature.properties['marker-symbol'] == id || id == 'all';
+        markerLayer.add_feature({
+        geometry: {coordinates: [-122.32, 47.61]},
+        properties: {'marker-color': '#000',
+                      'marker-size': "medium",
+                      'marker-symbol': "bus",
+                      title: 'Bar One',
+                      description: 'Happy Hour blah blah.'}
         });
-        return false;
-    }
-});
+        markerLayer.add_feature({
+        geometry: {coordinates: [-122.4, 47.58]},
+        properties: {'marker-color': '#000',
+                      'marker-size': "medium",
+                      'marker-symbol': "bus",
+                      title: 'Bar Two',
+                      description: 'Happy Hour c.'}
+        });
+        markerLayer.add_feature({
+        geometry: {coordinates: [-122.38, 47.55]},
+        properties: {'marker-color': '#000',
+                    'marker-size': "medium",
+                    'marker-symbol': "bus",
+                    title: 'Bar Three',
+                    description: 'Happy Hour rulezzzz'}
+        });
+       markerLayer.add_feature({
+        geometry: {coordinates: [lngi, lati]},
+        properties: {'marker-color': '#E01B6A',
+                    'marker-size': "medium",
+                    'marker-symbol': "bus",
+                    title: 'You',
+                    description: 'Here you are.'}
+
+        });
+
+        map.addLayer(markerLayer)
+          .setExtent(markerLayer.extent());
+
+
+        // Add share control
+        mapbox.share().map(map).add();
+
+        // Set title and description from tilejson
+        document.title = tilejson.name;
+        $('h1.map-title').text(tilejson.name);
+        $('p.description').text(tilejson.description);
+
+
+        var container = $('#markerfilters');
+        $.each(tilejson.markers.markers(), function(index, m) {
+            var s = m.data.properties['marker-symbol'];
+
+            if (container.find('[href="#' + s + '"]').length) return;
+
+            var el = $(document.createElement('a'))
+                .addClass('markerfilter')
+                .attr('href', '#' + s)
+                .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-'+s+'+000000.png)')
+                .bind('click', filter);
+            container.append(el);
+        });
+
+
+        $('[href="#all"]').bind('click', filter);
+
+
+        function filter(e) {
+            container.find('a').removeClass('selected');
+            var id = $(this).addClass('selected').attr('href').replace('#', '');
+            tilejson.markers.filter(function(feature) {
+                return feature.properties['marker-symbol'] == id || id == 'all';
+            });
+            return false;
+        }
+  });
 }
