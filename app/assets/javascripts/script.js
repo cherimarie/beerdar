cc939936c7f959c37dca41990042018afeff54f6
cherimarie <cherimarie@gmail.com>
2013-05-23 09:53:13 -0700

    Tidying code, no real changes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -24,9 +24,9 @@
     $('#map').mapbox('beerdar.map-97fds4u6', function(map, tilejson)
     {
 
-        map.setZoomRange(13, 19);
-        var markerLayer = mapbox.markers.layer();
-        mapbox.markers.interaction(markerLayer);
+      map.setZoomRange(13, 19);
+      var markerLayer = mapbox.markers.layer();
+      mapbox.markers.interaction(markerLayer);
 
       //these hard-coded arrays represent the data that will be sent in about 10 nearest bars
        //then function runs through, creating markers
@@ -82,40 +82,40 @@
         .setExtent(markerLayer.extent());
 
 
-        // Add share control
-        mapbox.share().map(map).add();
+      // Add share control
+      mapbox.share().map(map).add();
 
-        // Set title and description from tilejson
-        document.title = tilejson.name;
-        $('h1.map-title').text(tilejson.name);
-        $('p.description').text(tilejson.description);
-
-
-        var container = $('#markerfilters');
-        $.each(tilejson.markers.markers(), function(index, m) {
-            var s = m.data.properties['marker-symbol'];
-
-            if (container.find('[href="#' + s + '"]').length) return;
-
-            var el = $(document.createElement('a'))
-                .addClass('markerfilter')
-                .attr('href', '#' + s)
-                .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-'+s+'+000000.png)')
-                .bind('click', filter);
-            container.append(el);
-        });
+      // Set title and description from tilejson
+      document.title = tilejson.name;
+      $('h1.map-title').text(tilejson.name);
+      $('p.description').text(tilejson.description);
 
 
-        $('[href="#all"]').bind('click', filter);
+      var container = $('#markerfilters');
+      $.each(tilejson.markers.markers(), function(index, m) {
+          var s = m.data.properties['marker-symbol'];
+
+          if (container.find('[href="#' + s + '"]').length) return;
+
+          var el = $(document.createElement('a'))
+              .addClass('markerfilter')
+              .attr('href', '#' + s)
+              .css('background-image', 'url(http://a.tiles.mapbox.com/v3/marker/pin-l-'+s+'+000000.png)')
+              .bind('click', filter);
+          container.append(el);
+      });
 
 
-        function filter(e) {
-            container.find('a').removeClass('selected');
-            var id = $(this).addClass('selected').attr('href').replace('#', '');
-            tilejson.markers.filter(function(feature) {
-                return feature.properties['marker-symbol'] == id || id == 'all';
-            });
-            return false;
-        }
+      $('[href="#all"]').bind('click', filter);
+
+
+      function filter(e) {
+          container.find('a').removeClass('selected');
+          var id = $(this).addClass('selected').attr('href').replace('#', '');
+          tilejson.markers.filter(function(feature) {
+              return feature.properties['marker-symbol'] == id || id == 'all';
+          });
+          return false;
+      }
   });
 }
