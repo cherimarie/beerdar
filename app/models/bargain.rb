7f9525316a025cf34f1643030a3d8d4bcafb7bdf
David Husa <davidhusa@gmail.com>
2013-06-02 02:33:02 -0700

    add test data (load with rake db:data:load) and added support for narrowing down queries to currently running happy hours

--- app/models/bargain.rb
+++ app/models/bargain.rb
@@ -1,5 +1,5 @@
 class Bargain < ActiveRecord::Base
-  belongs_to :happy_hour, dependent: :destroy
+  belongs_to :happy_hour
   attr_accessible :deal, :deal_type, :discount_or_price, :version, :record_number
 
 end
--- app/models/happy_hour.rb
+++ app/models/happy_hour.rb
@@ -1,6 +1,68 @@
 class HappyHour < ActiveRecord::Base
-  has_many :bargains
-  belongs_to :location, dependent: :destroy
-  attr_accessible :days, :end_time, :name, :start_time, :version, :record_number
+  has_many :bargains, dependent: :destroy
+  belongs_to :location
+  attr_accessible :days, :end_time, :name, :start_time, :version, :record_number, :same_bargains
+  scope :open_today, -> { where("days like '%#{self.todays_letter}%'") }
 
+  def show_bargains
+    if same_bargains
+    begin
+      Bargain.where('happy_hour_id = ?', same_bargains).all
+    rescue ActiveRecord::RecordNotFound
+      [ Bargain.new { |x| x.deal = "same_bargain is not valid happy hour id. Happy Hour not found" } ]
+    end
+    else
+      bargains
+    end
+  end
+
+  def self.todays_letter
+    day_of_the_week = Time.now.wday
+    case day_of_the_week
+    when 0
+      "U" #Sunday
+    when 1
+      "M" #Monday
+    when 2
+      "T" #Tuesday
+    when 3
+      "W" #Wednesday
+    when 4
+      "H" #Thursday
+    when 5
+      "F" #Friday
+    when 6
+      "S" #Saturday
+    end
+  end
+
+  def open_now?
+    open_today? && open_at_this_time?
+  end  
+
+  def open_today?
+    days.include?(HappyHour.todays_letter)
+  end
+
+  def open_at_this_time?
+    time_inside?(Time.now.utc, start_time.utc, end_time.utc)
+  end
+  
+private
+# converts datetime object to float containing the time w/ minutes as decimals (e.g. 5:30 => 5.30)
+  def t2f(time) 
+    time.strftime('%H.%M').to_f
+  end
+
+  def time_inside?(time, start, stop)
+    if t2f(stop) < t2f(start)
+      if t2f(time) >= t2f(start)
+        t2f(time) >= t2f(stop)
+      elsif t2f(time) <= t2f(start)
+        t2f(time) <= t2f(stop)
+      end
+    else
+      t2f(start) < t2f(time) && t2f(time) < t2f(stop) 
+    end
+  end
 end
