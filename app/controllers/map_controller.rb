66cf3f17b8107376b9f56890c6c8b95cf5c72318
David Husa <davidhusa@gmail.com>
2013-06-09 20:52:52 -0700

    add static map to list view, remove redundant titles from list, add zoom and address params

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,6 @@
 class MapController < ApplicationController
+  helper_method :get_user_location
+
   def index
     options = {
       current_location: get_user_location,
@@ -8,22 +10,35 @@ class MapController < ApplicationController
   end
 
   def list
-    @list_data = Location.near(get_user_location)
-     .includes(:happy_hours)
-     .includes(:happy_hours => :bargains)
+    location_data = Location.near(get_user_location).limit(params[:limit] || 50)
+       .includes(:happy_hours)
+       .includes(:happy_hours => :bargains)
+    
+    unless params[:view_all] == "y"
+      @list_data = []
+      location_data.each_with_index do |location, index|
+        @list_data << location if location.has_open_happy_hour?
+      end
+    else
+      @list_data = location_data
+    end
   end
 
   def welcome;end
 
   private
-    def get_user_location
-      if cookies[:lat_lng]
+    def get_user_location(as_string=false)
+      if params[:address]
+        geolocation = Geocoder.search(params[:address])
+        lat_lng = [geolocation[0].latitude, geolocation[0].longitude]
+      elsif cookies[:lat_lng]
         lat_lng = cookies[:lat_lng].split("|").map(&:to_f)
       elsif request.location
         lat_lng = [request.location.latitude, request.location.longitude]
       else
         flash[:no_location_found] = "No location found"
       end
-      lat_lng
+      return lat_lng unless as_string
+      return "#{lat_lng[0]}, #{lat_lng[1]}"
     end
 end
--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -32,4 +32,20 @@ module MapHelper
     addy_array = address.split(",")
     return addy_array[0]
   end
+
+  def static_marker(location, label, color = "green")
+    return "" unless location
+    return "&markers=color:#{color}%7Clabel:#{label}%7C#{location.latitude},#{location.longitude}"
+  end
+
+  def mass_markers(location_data, number = 10)
+    number = 26 if number > 26
+    return_string = ""
+    number.times do |index|
+      return_string << static_marker(location_data[index], (index + 65).chr)
+    end
+    return_string
+  end
+  
+  # &markers=color:blue%7Clabel:S%7C40.702147,-74.015794
 end
